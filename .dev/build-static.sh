ff4b7fb18316cf269ff81206f712c76a92abed47
Marco Fontani <MFONTANI@cpan.org>
2023-01-21 17:19:16 +0100

    build: declare current dir as safe before building

--- .dev/build-static.sh
+++ .dev/build-static.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# fatal: detected dubious ownership in repository at '/__w/tstdin/tstdin'
+git config --global --add safe.directory "$(pwd)"
 go build -tags timetzdata --ldflags "-X 'main.Version=$(git describe --tags)' -extldflags \"-static\" -s -w" -o tstdin .
